11a6631894554aae10eeba4c162220a01f516117
Bjarne Riis <bjanre@briis.com>
2023-09-18 08:23:40 +0000

    Added attributes

--- custom_components/weatherflow_forecast/weather.py
+++ custom_components/weatherflow_forecast/weather.py
@@ -59,3 +59,9 @@ class WeatherFlowWeather(SingleCoordinatorWeatherEntity[WeatherFlowForecastDataU
     )
     _attr_has_entity_name = True
     _attr_native_temperature_unit = UnitOfTemperature.CELSIUS
+    _attr_native_precipitation_unit = UnitOfPrecipitationDepth.MILLIMETERS
+    _attr_native_pressure_unit = UnitOfPressure.HPA
+    _attr_native_wind_speed_unit = UnitOfSpeed.METERS_PER_SECOND
+    _attr_supported_features = (
+        WeatherEntityFeature.FORECAST_DAILY | WeatherEntityFeature.FORECAST_HOURLY
+    )
