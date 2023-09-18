42b9f5435c8a14c72a1dd8537f5e8c7f514fbaef
Bjarne Riis <bjarne@briis.com>
2023-09-18 06:11:45 +0000

    Added session to init

--- custom_components/weatherflow_forecast/__init__.py
+++ custom_components/weatherflow_forecast/__init__.py
@@ -101,7 +101,7 @@ class WeatherFlowForecastWeatherData:
     def initialize_data(self) -> bool:
         """Establish connection to API."""
         self._weather_data = WeatherFlow(
-            self._config[CONF_STATION_ID], self._config[CONF_API_TOKEN])
+            self._config[CONF_STATION_ID], self._config[CONF_API_TOKEN], session=async_get_clientsession(self.hass))
 
         return True
 
